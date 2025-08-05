dbcc836749924d6f0a1c0169d0431c839e0f5374
GustavoRosa-Dev <gustavohmrosa@gmail.com>
2025-08-05 16:12:10 -0300

    Classes Compostas

--- Java POO/Aula07e08/src/aula07e08/Aula07e08.java
+++ Java POO/Aula07e08/src/aula07e08/Aula07e08.java
@@ -5,19 +5,16 @@
 package aula07e08;
 public class Aula07e08 {
     public static void main(String[] args) {
-        Lutador l1 = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        Lutador[] lutadores = new Lutador[5];
+
+        lutadores[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        lutadores[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        lutadores[2] = new Lutador("Snapshadow", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
+        lutadores[3] = new Lutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        lutadores[4] = new Lutador("Ufocobol", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
         
-        Lutador l2 = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
-        
-        Lutador l3 = new Lutador("Snapshadow", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
-        
-        Lutador l4 = new Lutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
-        
-        Lutador l5 = new Lutador("Ufocobol", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
-        
-        
-        l1.apresentar();
-        l1.status();
+        Luta UEC01 = new Luta(lutadores[0], lutadores[1]);
+        UEC01.marcarLuta(lutadores[0], lutadores[1]);
+        UEC01.lutar();
     }
-    
 }
--- Java POO/Aula07e08/src/aula07e08/Luta.java
+++ Java POO/Aula07e08/src/aula07e08/Luta.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package aula07e08;
+import java.util.Random;
 public class Luta {
     //atributos
     private Lutador desafiado;
@@ -10,11 +11,84 @@ public class Luta {
     private int rounds;
     private boolean aprovada;
     
+    //construtor getters e setters
+
+    public Luta(Lutador desafiado, Lutador desafiante) {
+        this.desafiado = desafiado;
+        this.desafiante = desafiante;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
+    
     //metodos
-    public void marcarLuta(){
-        
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1!=l2){
+            aprovada = true;
+            desafiado = l1;
+            desafiante = l2;
+        } else {
+            aprovada = false;
+            desafiado = null;
+            desafiante = null;
+            System.out.println("Essa luta não foi aprovada");
+        }
     }
     public void lutar(){
-        
+        if(isAprovada()){
+            desafiado.apresentar();
+            desafiante.apresentar();
+            
+            Random random = new Random();
+            int vencedor = random.nextInt(3);
+            
+            switch (vencedor) {
+            case 0:
+                System.out.println("Empate!");
+                desafiado.empatarLuta();
+                desafiante.empatarLuta();
+                break;
+            case 1:
+                System.out.println(desafiado.getNome() + " venceu!");
+                desafiado.ganharLuta();
+                break;
+            case 2:
+                System.out.println(desafiante.getNome() + " venceu!");
+                desafiante.ganharLuta();
+                break;
+            }
+        } else {
+            System.out.println("A luta não foi aprovada");
+        }
     }
 }
