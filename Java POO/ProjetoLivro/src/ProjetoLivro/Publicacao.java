057e32c25eec4362096fe4c2b15790a56f501a27
GustavoRosa-Dev <gustavohmrosa@gmail.com>
2025-08-19 15:51:35 -0300

    Projeto Livro

---
+++ Java POO/ProjetoLivro/src/ProjetoLivro/Publicacao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ProjetoLivro;
+public interface Publicacao {
+    void abrir();
+    void fechar();
+    void folhear(int pagina);
+    void avançarPag();
+    void voltarPag();
+}
